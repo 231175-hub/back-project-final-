78909477a56631984533f8c8b3824bacefffd653
Codideep <supportaccount@codideep.com>
2026-07-19 20:55:22 -0500

    ya sa

--- src/main/java/com/epiis/finalproject/config/WebConfig.java
+++ src/main/java/com/epiis/finalproject/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.epiis.finalproject.config;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get("storage").toAbsolutePath().normalize();
+        String uploadPath = uploadDir.toUri().toString();
+        if (!uploadPath.endsWith("/")) {
+            uploadPath += "/";
+        }
+        
         registry.addResourceHandler("/storage/**")
-                .addResourceLocations("file:///D:/Proyectos Java/JavaProjectFinal/Java/finalproject/storage/");
+                .addResourceLocations(uploadPath);
     }
 }
